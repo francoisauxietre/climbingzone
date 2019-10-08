b3920fd72c5795bfd1b2d99d5459801df39d3904
krakenfrancois <f.auxietre@gmail.com>
2019-10-08 11:43:58 +0200

    add application.properties to connect to mysql database in local

--- zone/src/main/java/com/climbing/zone/ZoneApplication.java
+++ zone/src/main/java/com/climbing/zone/ZoneApplication.java
@@ -2,10 +2,18 @@ package com.climbing.zone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+// modification pour le docker de public class ZoneApplication extends SpringBootServletInitializer
 @SpringBootApplication
 public class ZoneApplication {
 
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(ZoneApplication.class);
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(ZoneApplication.class, args);
 	}
