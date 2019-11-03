84ce1aaa87bd50edfe0394bf74eda92a5d3e04c2
f.Auxietre <f.Auxietre@gmail.com>
2019-11-03 16:39:22 +0100

    Start with Adding User and privilège for the web service on spring

--- zone/src/main/java/com/climbing/zone/security/SecurityConfiguration.java
+++ zone/src/main/java/com/climbing/zone/security/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws  Exception{
         http
                 .authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
+//                .antMatchers("/", "/documentation/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
