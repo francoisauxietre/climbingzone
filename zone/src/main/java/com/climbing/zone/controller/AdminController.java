12749689bcec1d7497314f20f3f5d8736bca5521
f.Auxietre <f.Auxietre@gmail.com>
2019-11-17 11:40:19 +0100

    Add a Proxy handler on the Admin controller

--- zone/src/main/java/com/climbing/zone/controller/AdminController.java
+++ zone/src/main/java/com/climbing/zone/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.climbing.zone.controller;
 
-
+import com.climbing.zone.configuration.DynamicInvocationHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -16,8 +19,27 @@ public class AdminController {
     @GetMapping("index")
     public String index() {
         log.info("rentre dans AdminController");
-
+        //test du proxy
+        map.put("hello", "world");
+        map.get("hello");
         return "admin/index";
     }
+
+    //    Map map = (Map) Proxy.newProxyInstance(
+//            AdminController.class.getClassLoader(),
+//            new Class[] { Map.class },
+//            new DynamicInvocationHandler());
+    Map map = (Map) Proxy.newProxyInstance(
+            AdminController.class.getClassLoader(),
+            new Class[]{Map.class},
+            (proxy, method, methodArgs) -> {
+                if (method.getName().equals("get")) {
+                    return 42;
+                } else {
+                    log.info("methode " + method.getName() +" non supportee ici !");
+                    // throw new UnsupportedOperationException(
+                }
+                return proxy;
+            });
 }
 
